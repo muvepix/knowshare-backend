0381e0d5428b02f6a3dfd9af532a05a2496a8111
joaobenthin <joaopaulobenthin@hotmail.com>
2019-10-19 19:40:23 -0300

    create user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,42 +2,13 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    const userExists = await User.findOne({ email: req.body.email });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const { id, name, email, provider } = await User.create(req.body);
-
-    return res.json({
-      id,
-      name,
-      email,
-      provider,
-    });
-  }
-
-  async update(req, res) {
-    const { email, oldPassword } = req.body;
-
-    const user = await User.findById(req.userId);
-
-    if (email !== user.email) {
-      const userExists = await User.findOne({ where: { email } });
-
-      if (userExists) {
-        return res.status(400).json({ error: 'User already exists.' });
-      }
-    }
-
-    if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({ error: 'Password does not match' });
-    }
-
-    await user.update(req.body);
-
-    const { id, name } = await User.findById(req.userId);
+    const { id, name, email } = await User.create(req.body);
 
     return res.json({
       id,
