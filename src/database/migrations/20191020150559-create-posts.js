0881860972287e470f5553c51458bde5e7db013d
joaobenthin <joaopaulobenthin@hotmail.com>
2019-10-20 13:01:41 -0300

    heroku postgres

---
+++ src/database/migrations/20191020150559-create-posts.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      hashtags: {
+        type: Sequelize.ARRAY[Sequelize.STRING],
+        allowNull: true,
+      },
+      link: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        default: 1,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('posts');
+  },
+};
